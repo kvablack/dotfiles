43dcd7c39f09aeadf602bd13254b29f1b6f42c5f
Kevin Black <kevin3.black@gmail.com>
2023-11-03 14:53:48 -0700

    Add stack indicator for sketchybar, update bindings

---
+++ sketchybar/items/stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+stack=(
+  script="$PLUGIN_DIR/stack.sh"
+  drawing=on
+  update_freq=0
+  label.font="$FONT:Regular:14.0"
+)
+
+sketchybar -m --add item stack left \
+              --set stack "${stack[@]}" \
+              --subscribe stack window_focus front_app_switched space_change
---
+++ sketchybar/plugins/stack.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+TYPE=$(yabai -m query --spaces --space | jq '.["type"]')
+
+if [[ "$TYPE" = '"stack"' ]]; then
+    CURRENT=$(yabai -m query --windows --window | jq '.["stack-index"]')
+
+    declare -a dots=()
+    if [[ $CURRENT -eq 0 ]]; then
+        # Single window
+        dots+="●"
+    else
+        # Multiple window
+        LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
+
+        for i in $(seq 0 $(expr $LAST - 1))
+        do  
+          # Theme 1
+          # if [[ $i -lt $(expr $CURRENT - 1) ]]; then
+            # dots+="◖"
+          # elif [[ $i -gt $(expr $CURRENT - 1) ]]; then
+            # dots+="◗"
+          # elif [[ $i -eq $(expr $CURRENT - 1) ]]; then
+            # dots+="●"
+          # fi
+          # Theme 2
+          [[ $( expr $CURRENT - 1) -eq $i ]] && dots+="●" || dots+="○"
+        done
+    fi
+
+    sketchybar --set stack label=$(printf "%s" ${dots[@]})
+else
+    sketchybar --set stack label=""
+fi
