e9e65f7f2362af4bc239979a59c7d8342865d9a2
Kevin Black <kevin3.black@gmail.com>
2025-03-12 20:19:44 -0700

    Improve zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,9 +2,10 @@
 
 sh -c "RUNZSH=no; $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cd ~
-rm -f ~/.zshrc
-ln -s dotfiles/zsh/.zshrc .zshrc
-ln -s dotfiles/zsh/.p10k.zsh .p10k.zsh
+rm -f $HOME/.zshrc $HOME/.p10k.zsh
+ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 
-zsh -lic 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k'
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+    zsh -lic 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k'
+fi
