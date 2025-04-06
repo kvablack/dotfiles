9a968120362bbb71993461d74971ee0c683e9226
Kevin Black <kevin3.black@gmail.com>
2025-04-06 11:09:28 -0700

    Direct install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+DOTFILES_DIR=$HOME/.kevin-dotfiles
+
+if [ ! -d "$DOTFILES_DIR" ]; then
+    git clone https://github.com/kvablack/dotfiles $DOTFILES_DIR
+fi
+
 sh -c "RUNZSH=no; $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-rm -f $HOME/.zshrc $HOME/.p10k.zsh
-ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
+ln -s $DOTFILES_DIR/zsh/.zshrc $HOME/.zshrc
+ln -s $DOTFILES_DIR/zsh/.p10k.zsh $HOME/.p10k.zsh
 
 if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     zsh -lic 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k'
